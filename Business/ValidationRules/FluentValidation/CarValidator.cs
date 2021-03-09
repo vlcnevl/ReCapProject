555054c5a67a6d144fbf5fd4cf6e792b02aae3ab
VELİ CAN EVLİ <33234437+vlcnevl@users.noreply.github.com>
2021-03-09 20:40:28 +0300

    Autofac,FluentValidation and AOP added

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.CarId).NotEmpty();
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+            RuleFor(c => c.BrandId).NotEmpty();
+            RuleFor(c => c.ColorId).NotEmpty();
+            RuleFor(c => c.Description).MinimumLength(3);
+            RuleFor(c => c.DailyPrice).GreaterThanOrEqualTo(1000).When(p => p.BrandId == 2);
+
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public CustomerValidator()
+        {
+            RuleFor(c => c.CustomerName).NotEmpty().MinimumLength(3);
+            RuleFor(c => c.CustomerId).NotEmpty().GreaterThan(0);
+            RuleFor(c => c.UserId).NotEmpty().GreaterThan(0);
+        }
+    }
+}
